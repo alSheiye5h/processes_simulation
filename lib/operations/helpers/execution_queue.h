f4c97bbb79d4ca11bab7a260eaec6f5dcbcbf8c6
anas <anasbelkhadir13@gmail.com>
2025-12-20 16:06:52 +0100

    cooking

--- lib/operations/helpers/execution_queue.h
+++ lib/operations/helpers/execution_queue.h
@@ -1,10 +1,11 @@
 
 
 #include <structs/process.h>
+#include <structs/schedular.h>
 
 
-bool op_check_instruction_disponibility(INSTRUCTION* instruction) {
-
-
+bool op_check_instruction_disponibility(ORDONNANCEUR* schedular, INSTRUCTION* instruction) {
+    
+    return schedular->check_instruction_disponibility(instruction);
 
 }
--- lib/structs/ressource.h
+++ lib/structs/ressource.h
@@ -3,12 +3,16 @@
 #include <stdbool.h>
 
 typedef enum {
+
     AAA, BBB, CCC, DDD, EEE, FFF
+
 } RESSOURCE;
 
 typedef struct {
+    
     char ressource_name[10]; // nom de ressource
     RESSOURCE ressource; // type 
-    bool disponibilite; // dispo ou non 1 / 0
+    bool disponibilite; // dispo ou non true / false
     struct RESSOURCE_ELEMENT* next_ressource;
+
 } RESSOURCE_ELEMENT;
